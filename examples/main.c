135514c78114c79f70c6c8f56ea920bf2aa4f34f
rtulip <robert.tulip97@gmail.com>
2020-10-07 15:12:42 -0700

    Inital Commit
    This is the inital code for the 7.5in Black/White EPD from Waveshare.

---
+++ examples/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdlib.h>     //exit()
+#include <signal.h>     //signal()
+#include "EPD_Test.h"   //Examples
+
+void  Handler(int signo)
+{
+    //System Exit
+    printf("\r\nHandler:exit\r\n");
+    DEV_Module_Exit();
+
+    exit(0);
+}
+
+int main(void)
+{
+    // Exception handling:ctrl + c
+    signal(SIGINT, Handler);
+    
+    EPD_7in5_V2_test();
+    
+    return 0;
+}
